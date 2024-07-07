0dc30baf8e31564b4ecd6e86d439e2ac115c38ad
Matteo Bianchetti <mttbnchtt@gmail.com>
2024-07-07 07:25:46 -0400

    add notes to functions_heuristics

---
+++ tools/venv/code/functions_heuristics.py
@@ -0,0 +1,16 @@
+#############################################
+## HEURISTIC SEARCHES
+#############################################
+# find the goal of the proof
+
+# extract concepts from the goal of the proof
+
+# extract encyclopedic knowledge (= conceptual space) related to the the goal
+
+# organize conceptual space along heuristic dimensions (concepts, results, examples)
+
+# apply strategic knowledge
+# 1. select concepts for working memory
+# 2. select heuristic techniques
+# 3. apply means-ends analysis: asses difference between the current problem state and the possible future problem state after the heuristic enrichment and evaluate the difference with respect to the goal
+
