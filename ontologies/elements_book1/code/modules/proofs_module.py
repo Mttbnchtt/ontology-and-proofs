549278a8f55b2ce3ddbbe174c0f8efdf0fac13fa
Matteo Bianchetti <mttbnchtt@gmail.com>
2024-12-30 09:47:56 -0500

    Auto-commit at 2024-12-30 09:47:56; modified: ontologies/elements_book1/code/modules/proofs_module.py

--- ontologies/elements_book1/code/modules/proofs_module.py
+++ ontologies/elements_book1/code/modules/proofs_module.py
@@ -1,100 +1,86 @@
 import pandas as pd
 import rdflib
 
-RELATIONS_INPUT_FILE_PATH = "input/Euclid.RelationsAnalysis.Book1.csv"
-OPERATIONS_INPUT_FILE_PATH = "input/Euclid.OperationsAnalysis.Book1.csv"
+import modules.utils as utils
+
+PROOFS_INPUT_FILE_PATH = "input/Euclid.Proofs.Book1.csv"
 
 ONTOLOGY_NAMESPACE = "https://www.foom.com/core"
-CONCEPT_CLASS = utils.create_iri("Concept", namespace=ONTOLOGY_NAMESPACE)
-CONTAINS_CONCEPT = utils.create_iri("contains concept", namespace=ONTOLOGY_NAMESPACE)
-IS_CONCEPT_IN = utils.create_iri("is concept in", namespace=ONTOLOGY_NAMESPACE)
-OPERATION_INSTANCE_CLASS = utils.create_iri("Operation instance", namespace=ONTOLOGY_NAMESPACE)
-OPERATION_TYPE_CLASS = utils.create_iri("Operation type", namespace=ONTOLOGY_NAMESPACE)
-RELATION_INSTANCE_CLASS = utils.create_iri("Relation instance", namespace=ONTOLOGY_NAMESPACE)
-RELATION_TYPE_CLASS = utils.create_iri("Relation type", namespace=ONTOLOGY_NAMESPACE)
+PROOF_CLASS = utils.create_iri("Proof", namespace=ONTOLOGY_NAMESPACE)
+CONTAINS_CONCEPT = utils.create_iri("contains concept", namespace="https://www.foom.com/core")
+IS_CONCEPT_IN = utils.create_iri("is concept in", namespace="https://www.foom.com/core")
+REFERS_TO = utils.create_iri("refers to", namespace="https://www.foom.com/core")
+IS_USED_IN = utils.create_iri("is used in", namespace="https://www.foom.com/core")
+USES_REDUCTION = utils.create_iri("uses_reductio", namespace=ONTOLOGY_NAMESPACE)
 
-def add_relations_operations(kg: rdflib.Graph,
-                             input_file_path: str,
-                             item_type: typing.Literal["relations", "operations"]) -> rdflib.Graph:
-    items_df = pd.read_csv(input_file_path).fillna("")
-    for _, row in items_df.iterrows():
+def add_proofs(kg: rdflib.Graph,
+               input_file_path: str) -> rdflib.Graph:
 
-        if item_type == "relations":
-            instance_pref_label = row["relation_instance"].strip().capitalize()
-            type_pref_label = row["relation_type"].strip().capitalize()
-            kg, instance_iri, type_iri = add_relation_instance_type(kg, instance_pref_label, type_pref_label, "Relation instance", "Relation type")
+    # read database of proofs
+    proofs_df = pd.read_csv(input_file_path).fillna("")
+    for _, row in proofs_df.iterrows():
+        # add proof
+        kg, proof_iri = add_proof(kg, row["proof"])
 
-            # find concepts in instance and in type and add them to the graph
-            kg = add_concepts(kg, instance_iri, instance_pref_label)
-            kg = add_concepts(kg, type_iri, type_pref_label)
+        # add concepts
+        if concepts := row["additional_proof_concepts"]:
+            kg = add_concepts(kg, proof_iri, concepts)
 
-        elif item_type == "operations":
-            instance_pref_label = row["operation_instance"].strip().capitalize()
-            type_pref_label = row["operation_type"].strip().capitalize()
-            kg, instance_iri, type_iri = add_operation_instance_type(kg, instance_pref_label, type_pref_label, "Operation instance", "Operation type")
+        # add relation instance
+        if relation_instance := row["relation_instance"]:
+            kg = add_relation_instance(kg, relation_instance.strip(), proof_iri)
 
-            # find concepts in instance and in type and add them to the graph
-            kg = add_concepts(kg, instance_iri, instance_pref_label)
-            kg = add_concepts(kg, type_iri, type_pref_label)
+        # add operation instance
+        if operation_instance := row["operation_instance"]:
+            kg = add_operation_instance(kg, operation_instance.strip(), proof_iri)
 
-        else:
-            raise ValueError(f"Invalid item type: {item_type}")
+        # add implicit operation instance
+        if implicit_operation_instance := row["implicit_operation_instance"]:
+            kg = add_operation_instance(kg, implicit_operation_instance.strip(), proof_iri)
+
+        # add uses_reductio
+        if uses_reductio := row["reductio"]:
+            kg.add((proof_iri, USES_REDUCTION, rdflib.Literal("true", datatype=rdflib.XSD.boolean)))
 
     return kg
 
-def find_concepts(item_pref_label: str) -> set:
-    item_pref_label_v1 = item_pref_label.replace("(", " ").replace(")", " ").replace(",", " ")
 
-    return {concept.strip() for concept in item_pref_label_v1.split()}
+def add_proof(kg: rdflib.Graph,
+              proof_number: str) -> rdflib.Graph:
+    proof_label = f"Proof {proof_number}"
+    proof_iri = utils.create_iri(proof_label, namespace=ONTOLOGY_NAMESPACE)
+    kg.add((proof_iri, rdf_type, PROOF_CLASS))
+    kg.add((proof_iri, rdfs_label, rdflib.Literal(proof_label)))
+    kg.add((proof_iri, skos_prefLabel, rdflib.Literal(proof_number)))
 
+    return kg, proof_iri
 
 def add_concepts(kg: rdflib.Graph,
-                 item_iri: rdflib.URIRef,
-                 item_pref_label: str) -> rdflib.Graph:
-    concepts = find_concepts(item_pref_label)
-
-    for concept in concepts:
+                 proof_iri: rdflib.URIRef,
+                 concepts: str) -> rdflib.Graph:
+    concepts_list = [concept.strip() for concept in concepts.split(",")]
+    for concept in concepts_list:
         concept_iri = utils.create_iri(f"Concept: {concept}", namespace=ONTOLOGY_NAMESPACE)
-        kg.add((item_iri, CONTAINS_CONCEPT, concept_iri))
-        kg.add((concept_iri, IS_CONCEPT_IN, item_iri))
+        kg.add((proof_iri, CONTAINS_CONCEPT, concept_iri))
+        kg.add((concept_iri, IS_CONCEPT_IN, proof_iri))
 
     return kg
 
-def add_basic_triples(kg: rdflib.Graph,
-                      pref_label: str,
-                      class_iri: rdflib.URIRef,
-                      prefix: str) -> rdflib.Graph:
-    item_label = f"{prefix}: {pref_label}"
-    item_iri = utils.create_iri(item_label, namespace=ONTOLOGY_NAMESPACE)
-    kg.add((item_iri, rdf_type, class_iri))
-    kg.add((item_iri, rdfs_label, rdflib.Literal(item_label)))
-    kg.add((item_iri, skos_prefLabel, rdflib.Literal(pref_label)))
+def add_relation_instance(kg: rdflib.Graph,
+                          relation_instance: str,
+                          proof_iri: rdflib.URIRef) -> rdflib.Graph:
+    relation_instance_iri = utils.create_iri(f"Relation instance: {relation_instance}", namespace=ONTOLOGY_NAMESPACE)
+    kg.add((proof_iri, REFERS_TO, relation_instance_iri))
+    kg.add((relation_instance_iri, IS_USED_IN, proof_iri))
+            
+    return kg
 
-    return kg, item_iri
-
-def add_relation_instance_type(kg: rdflib.Graph,
-                               relation_instance_pref_label: str,
-                               relation_type_pref_label: str,
-                               prefix_instance: str,
-                               prefix_type: str) -> rdflib.Graph:
-
-    kg, instance_iri = add_basic_triples(kg, relation_instance_pref_label, RELATION_INSTANCE_CLASS, prefix_instance)
-    kg, type_iri = add_basic_triples(kg, relation_type_pref_label, RELATION_TYPE_CLASS, prefix_type)
-
-    kg.add((instance_iri, HAS_RELATION_TYPE, type_iri))
-    kg.add((type_iri, IS_RELATION_TYPE_OF, instance_iri))
-
-    return kg, instance_iri, type_iri
-
-def add_operation_instance_type(kg: rdflib.Graph,
-                                operation_instance_pref_label: str,
-                                operation_type_pref_label: str,
-                                prefix_instance: str,
-                                prefix_type: str) -> rdflib.Graph:
-    kg, instance_iri = add_basic_triples(kg, operation_instance_pref_label, OPERATION_INSTANCE_CLASS, prefix_instance)
-    kg, type_iri = add_basic_triples(kg, operation_type_pref_label, OPERATION_TYPE_CLASS, prefix_type)
-
-    kg.add((instance_iri, HAS_RELATION_TYPE, type_iri))
-    kg.add((type_iri, IS_RELATION_TYPE_OF, instance_iri))
-
-    return kg, instance_iri, type_iri
+            
+def add_operation_instance(kg: rdflib.Graph,
+                           operation_instance: str,
+                           proof_iri: rdflib.URIRef) -> rdflib.Graph:
+    operation_instance_instance_iri = utils.create_iri(f"Operation instance: {operation_instance}", namespace=ONTOLOGY_NAMESPACE)
+    kg.add((proof_iri, REFERS_TO, operation_instance_instance_iri))
+    kg.add((operation_instance_instance_iri, IS_USED_IN, proof_iri))
+            
+    return kg
