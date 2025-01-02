ebd1148e742bcafb783579ec4a9a47db79f95c53
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-01-02 13:57:40 -0500

    Auto-commit at 2025-01-02 13:57:40; modified: ontologies/elements_book1/code/modules/operations_relations_module.py, ontologies/elements_book1/ontologies/euclid_book1.ttl

--- ontologies/elements_book1/code/modules/operations_relations_module.py
+++ ontologies/elements_book1/code/modules/operations_relations_module.py
@@ -81,13 +81,13 @@ def add_concepts(kg: rdflib.Graph,
                  item_iri: rdflib.URIRef,
                  item_pref_label: str) -> rdflib.Graph:
     concepts = find_concepts(item_pref_label)
-    print(concepts)
 
-    for concept in concepts:
-        if concept.replace(" ", "") and not concept in {"v", "w", "x" , "y", "z"}:
-            concept_iri = utils.create_iri(f"Concept: {concept}", namespace=ONTOLOGY_NAMESPACE)
-            kg.add((item_iri, CONTAINS_CONCEPT, concept_iri))
-            kg.add((concept_iri, IS_CONCEPT_IN, item_iri))
+    if concepts:
+        for concept in concepts:
+            if concept.replace(" ", "") and not concept in {"v", "w", "x" , "y", "z", "V", "W", "X", "Y", "Z"}:
+                concept_iri = utils.create_iri(f"Concept: {concept}", namespace=ONTOLOGY_NAMESPACE)
+                kg.add((item_iri, CONTAINS_CONCEPT, concept_iri))
+                kg.add((concept_iri, IS_CONCEPT_IN, item_iri))
 
     return kg
 
