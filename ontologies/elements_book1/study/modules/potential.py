48fc7543f7aa42f9ee926f7e0dde3991bfc9fc83
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-10-04 06:33:38 -0400

    Auto-commit at 2025-10-04 06:33:38; modified: ontologies/elements_book1/study/modules/potential.py

--- ontologies/elements_book1/study/modules/potential.py
+++ ontologies/elements_book1/study/modules/potential.py
@@ -2,6 +2,19 @@ import pandas as pd
 import rdflib
 
 
+
+def calculate_activation_potential(kg: rdflib.Graph,
+                                   proposition_number: int = 0):
+    # history potential
+    calculated_history_potential_df = history(kg, proposition_number)
+    # print(len(calculated_history_potential_df))
+    # print(calculated_history_potential_df["activation_potential"].sum())
+    # hebb potential
+    hebb_potential = hebb(kg, proposition_number)
+    # print(len(hebb_potential))
+    # print(hebb_potential["activation_potential"].sum())
+    return calculated_history_potential_df, hebb_potential
+
 def main(kg: rdflib.Graph,
          proposition_number: int = 1,
          given_upper_part: float = 1/4):
